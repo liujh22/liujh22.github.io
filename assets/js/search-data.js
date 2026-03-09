882938eefeafb4ee00cafb22e7aeacd22e79038d
liujh22 <liujh22@163.com>
2026-03-09 01:35:30 +0000

    Deploying to gh-pages from @ liujh22/liujh22.github.io@2a4d5c86d38cb5b12396562fd8034d9550c388e2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
@@ -66,12 +66,7 @@ ninja.data = [{
             window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
           
         },
-      },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{id: "news-my-blog-goes-online-today",
+      },{id: "news-my-blog-goes-online-today",
           title: 'My blog goes online today! 😄',
           description: "",
           section: "News",},{id: "projects-project-1",
